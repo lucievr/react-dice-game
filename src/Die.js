610a63368ad7b12e15d54a5610bb7733563c3138
Lucie <46979603+lucievr@users.noreply.github.com>
2019-06-27 01:19:58 +0200

    Add react icons and complete dice component

--- src/Die.js
+++ src/Die.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './Die.css';
+import { FaDiceOne, FaDiceTwo, FaDiceThree, FaDiceFour, FaDiceFive, FaDiceSix } from "react-icons/fa";
 
-function Die(props) {
-    let iconStr = `fas fa-dice-${props.dice}`
-    return (
-        <div>
-            <i className={iconStr}></i>
-        </div>
-    )    
-}
+class Die extends React.Component {
+      
+    render() {
+        const dice = this.props.dice
+        return (
+          <div>
+                {dice === 1 ? <FaDiceOne className="Die" /> : null}
+                {dice === 2 ? <FaDiceTwo className="Die" /> : null}
+                {dice === 3 ? <FaDiceThree className="Die" /> : null}
+                {dice === 4 ? <FaDiceFour className="Die" /> : null}
+                {dice === 5 ? <FaDiceFive className="Die" /> : null}
+                {dice === 6 ? <FaDiceSix className="Die" /> : null}
+          </div>
 
-export default Die;
+      );
+    }
+  }
+  export default Die;
--- src/RollDice.js
+++ src/RollDice.js
@@ -5,8 +5,9 @@ class RollDice extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            button: "Roll Dice!",
-            face: "one",
+            btnText: "Roll Dice!",
+            face1: 1,
+            face2: 4
         }
         this.handleClick = this.handleClick.bind(this);
     }
@@ -17,9 +18,9 @@ class RollDice extends React.Component {
     render() {
         return (
             <div>
-                <Die dice={this.state.face}/>
-                <Die dice={this.state.face}/>
-                <button onClick={this.handleClick}>{this.state.button}</button>
+                <Die dice={this.state.face1}/>
+                <Die dice={this.state.face2}/>
+                <button onClick={this.handleClick}>{this.state.btnText}</button>
             </div>
         )
     }
